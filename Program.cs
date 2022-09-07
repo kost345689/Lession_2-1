b3156229904a5b10f9466b48a9924a0f3e69d7b6
kostya <kostya_pgs@mail>
2022-09-07 07:39:12 +0700

    1

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+
+
+
+while(true)
+    {
+
+        Console.WriteLine("Ввведите трезначное число");
+        string input = Console.ReadLine();
+        if(!input.Equals("exit"))
+            Console.WriteLine("{0}->{1}",input, input[1]);
+        else
+            break;
+    }
+
+
